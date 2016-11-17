dc0404af9520a6c73cc82a501ea447acd5f88498
PJC-1 <jumpzine@gmail.com>
2016-11-16 22:28:00 -0800

    set up two child component files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class extends Component {
+export default class extends React.Component {
   constructor(props) {
     super(props);
 
